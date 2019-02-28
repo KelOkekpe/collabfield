505ff6bd66dce9a3806eae53b26c6d7a2440e3c2
joseph@callrail.com <jokekpe@MacBook-Pro-106.localdomain>
2019-02-28 15:31:40 -0500

    - Add the bootstrap layout to the home page - Add CSS to make home page layout's stylistic and responsive design changes

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -12,22 +12,22 @@ end
 
 #These are alternative ways to write the same specs, previously these specs failed
 #So I found another way to write them. Once they passed, all my specs passed.
-RSpec.describe Category, type: :model do
-  context 'Associations' do
-      it { should_have_many(:posts) }
-    it 'has_many posts' do
-      association = described_class.reflect_on_association(:posts)
-      expect(association.macro).to eq :has_many
-    end
-  end
-
-
-RSpec.describe Category, type: :model do
-    context 'Associations' do
-      it 'should have many posts' do
-        subject { described_class.new }
-        assc = described_class.reflect_on_association(:posts)
-        expect(assc.macro).to eq :has_many
-      end
-    end
-  end
+# RSpec.describe Category, type: :model do
+#   context 'Associations' do
+#       it { should_have_many(:posts) }
+#     it 'has_many posts' do
+#       association = described_class.reflect_on_association(:posts)
+#       expect(association.macro).to eq :has_many
+#     end
+#   end
+#
+#
+# RSpec.describe Category, type: :model do
+#     context 'Associations' do
+#       it 'should have many posts' do
+#         subject { described_class.new }
+#         assc = described_class.reflect_on_association(:posts)
+#         expect(assc.macro).to eq :has_many
+#       end
+#     end
+#   end
