a3d6e45f4ebc20b3039dcfdecd82e52ca1d3f3d3
joseph@callrail.com <jokekpe@MacBook-Pro-106.localdomain>
2019-02-28 15:16:05 -0500

    Add specs for User, Category, Post models' associations

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Unicorn as the app server
 # gem 'unicorn'
 
+gem 'pry', '~> 0.10.3'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Associations' do
+    it 'has_many posts' do
+      association = described_class.reflect_on_association(:posts)
+      expect(association.macro).to eq :has_many
+    end
+  end
 end
+
+
+#These are alternative ways to write the same specs, previously these specs failed
+#So I found another way to write them. Once they passed, all my specs passed.
+RSpec.describe Category, type: :model do
+  context 'Associations' do
+      it { should_have_many(:posts) }
+    it 'has_many posts' do
+      association = described_class.reflect_on_association(:posts)
+      expect(association.macro).to eq :has_many
+    end
+  end
+
+
+RSpec.describe Category, type: :model do
+    context 'Associations' do
+      it 'should have many posts' do
+        subject { described_class.new }
+        assc = described_class.reflect_on_association(:posts)
+        expect(assc.macro).to eq :has_many
+      end
+    end
+  end
