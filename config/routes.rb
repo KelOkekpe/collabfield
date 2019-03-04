29ddd3b354b15a076fb0da9b159ec95c9ced188f
joseph@callrail.com <jokekpe@MacBook-Pro-106.localdomain>
2019-03-04 14:49:55 -0500

    adding CSS style to modal did not work

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,29 @@
 Rails.application.routes.draw do
 
+  #root(homepage) goes to pages_controller and calls index method
   root to: 'pages#index'
 
-  devise_for :users, :controllers => {:registrations => "registrations"}
 
+    resources :posts do
+      collection do
+        get 'hobby'
+        get 'study'
+        get 'team'
+      end
+    end
 
+    #why doesnt there need to be a devise for logins?
+    devise_for :users, :controllers => {:registrations => "registrations"}
 
-  devise_scope :user do
-    get 'signup', to: 'devise/registrations#new'
+    #if the user clicks the signup link, this is where they will be directed
+    devise_scope :user do
+      get 'signup', to: 'devise/registrations#new'
+    end
+
+    #if user clicks the login link, this is where they will be directed
+    #why doesn't there need to be a sessions controller?
+    devise_scope :user do
+      get 'login', to: 'devise/sessions#new'
+    end
+
   end
-
-  devise_scope :user do
-    get 'login', to: 'devise/sessions#new'
-  end
-
-  resources :posts do
-  collection do
-    get 'hobby'
-    get 'study'
-    get 'team'
-  end
-end
-end
