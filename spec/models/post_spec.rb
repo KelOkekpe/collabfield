c5bbf213adab5578d7ca024d7a8f3b72f1de6660
joseph@callrail.com <jokekpe@MacBook-Pro-106.localdomain>
2019-03-15 10:49:21 -0400

    Add a spec for the Post model's default_scope

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -13,3 +13,11 @@ RSpec.describe Post, type: :model do
     end
   end
 end
+
+context 'Scopes' do
+  it 'default_scope orders by descending created_at' do
+    first_post = create(:post)
+    second_post = create(:post)
+    expect(Post.all).to eq [second_post, first_post]
+  end
+end
