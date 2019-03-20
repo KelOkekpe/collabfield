7153f946daa361ae735afc104219cca5e1f9c216
joseph@callrail.com <jokekpe@MacBook-Pro-106.localdomain>
2019-03-20 15:45:41 -0400

    Create a UI to create new posts - Inside the PostsController:   define new and create actions   define a post_params method   define a before_action filter - Inside the ApplicationController:   define a redirect_if_not_signed_in method   define a redirect_if_signed_in method - Create a new template for posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :redirect_if_not_signed_in, only: [:new]
+
+  def redirect_if_not_signed_in
+    redirect_to root_path if !user_signed_in?
+  end
+
+  def redirect_if_signed_in
+    redirect_to root_path if user_signed_in?
+  end
+
+
 end
